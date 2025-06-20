05561512d9c2ddadda08c9de2e1b08735095c9be
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2025-06-20 16:07:09 +0500

    feat: created and styled review section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
-import AboutMe from "@/components/complex/AboutMe";
-import Herosection from "@/components/complex/Herosection";
-import Portfolio from "@/components/complex/Portfolio";
-import Services from "@/components/complex/Services";
-import LeftBanner from "@/components/compound/LeftBanner";
-import MiddleBanner from "@/components/compound/MiddleBanner";
-import Personalnfo from "@/components/compound/Personalnfo";
-import RightBanner from "@/components/compound/RightBanner";
+import AboutMe from '@/components/complex/AboutMe'
+import Herosection from '@/components/complex/Herosection'
+import Portfolio from '@/components/complex/Portfolio'
+import Reviews from '@/components/complex/Reviews'
+import Services from '@/components/complex/Services'
+import LeftBanner from '@/components/compound/LeftBanner'
+import MiddleBanner from '@/components/compound/MiddleBanner'
+import Personalnfo from '@/components/compound/Personalnfo'
+import RightBanner from '@/components/compound/RightBanner'
 
-
-export default function Home() {
+export default function Home () {
   return (
-  <main>
-    <Herosection/>
-    <AboutMe/>
-    <Personalnfo/>
-    <Services/>
-    <Portfolio/>
-  </main>
-  );
+    <main>
+      <Herosection />
+      <AboutMe />
+      <Personalnfo />
+      <Services />
+      <Portfolio />
+      <Reviews />
+    </main>
+  )
 }
